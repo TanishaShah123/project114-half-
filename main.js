e84c2295ad736cab20ade2d016ef47ab77bd9f97
TanishaShah123 <88231485+TanishaShah123@users.noreply.github.com>
2022-04-08 17:46:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    myCanvas = createCanvas(700, 500);
+    myCanvas.center();
+}
+
+function draw() {
+
+}
+
+function clickimg() {
+    save("Img.png")
+}
